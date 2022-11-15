cb5ccd998003058857ace4036a18fcd562469e95
Khanh Nguyen <encacap@gmail.com>
2022-11-15 22:00:29 +0700

    feat(ERA-16): add cloudflare-variant seeder and fix other seeder issues

--- src/seeders/seeder.seeder.ts
+++ src/seeders/seeder.seeder.ts
@@ -1,11 +1,15 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { seeder } from 'nestjs-seeder';
 import { AppConfigModule } from 'src/configs/config.module';
+import { CloudflareVariantWebsiteEntity } from 'src/modules/cloudflare/entities/cloudflare-variant-website.entity';
+import { CloudflareVariantEntity } from 'src/modules/cloudflare/entities/cloudflare-variant.entity';
+import { ContactEntity } from 'src/modules/contact/entities/contact.entity';
 import { RoleEntity } from 'src/modules/user/entities/role.entity';
 import { UserRoleMappingEntity } from 'src/modules/user/entities/user-role-mapping.entity';
 import { UserEntity } from 'src/modules/user/entities/user.entity';
 import { WebsiteEntity } from 'src/modules/website/entities/website.entity';
 import { PostgresDatabaseProviderModule } from 'src/providers/postgres/postgres.module';
+import { CloudflareVariantSeeder } from './cloudflare-variant.seeder';
 import { RoleSeeder } from './role.seeder';
 import { UserSeeder } from './user.seeder';
 import { WebsiteSeeder } from './website.seeder';
@@ -14,6 +18,14 @@ seeder({
   imports: [
     AppConfigModule,
     PostgresDatabaseProviderModule,
-    TypeOrmModule.forFeature([WebsiteEntity, RoleEntity, UserEntity, UserRoleMappingEntity]),
+    TypeOrmModule.forFeature([
+      WebsiteEntity,
+      RoleEntity,
+      UserEntity,
+      UserRoleMappingEntity,
+      CloudflareVariantEntity,
+      CloudflareVariantWebsiteEntity,
+      ContactEntity,
+    ]),
   ],
-}).run([WebsiteSeeder, RoleSeeder, UserSeeder]);
+}).run([WebsiteSeeder, RoleSeeder, UserSeeder, CloudflareVariantSeeder]);
