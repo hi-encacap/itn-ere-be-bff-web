ecfe5cd57d4b76c9a9da2c03ab315d9e479dba41
Khanh Nguyen <encacap@gmail.com>
2022-11-22 14:15:10 +0700

    fix(ERA-23): resolve deepsource issues

--- src/common/utils/helpers.util.ts
+++ src/common/utils/helpers.util.ts
@@ -11,18 +11,17 @@ const randomStringPrefix = (separator?: string) => {
 const slugify = (text: string) => {
   let result = text.toLowerCase();
 
-  // xóa dấu
-  result = result.replace(/(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
-  result = result.replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
-  result = result.replace(/(ì|í|ị|ỉ|ĩ)/g, 'i');
-  result = result.replace(/(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
-  result = result.replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
-  result = result.replace(/(ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
-  result = result.replace(/(đ)/g, 'd');
+  result = result.replace(/(?<id>à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ)/g, 'a');
+  result = result.replace(/(?<id>è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ)/g, 'e');
+  result = result.replace(/(?<id>ì|í|ị|ỉ|ĩ)/g, 'i');
+  result = result.replace(/(?<id>ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ)/g, 'o');
+  result = result.replace(/(?<id>ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ)/g, 'u');
+  result = result.replace(/(?<id>ỳ|ý|ỵ|ỷ|ỹ)/g, 'y');
+  result = result.replace(/(?<id>đ)/g, 'd');
 
-  result = result.replace(/([^0-9a-z-\s])/g, '');
+  result = result.replace(/(?<id>[^0-9a-z-\s])/g, '');
 
-  result = result.replace(/(\s+)/g, '-');
+  result = result.replace(/(?<id>\s+)/g, '-');
 
   result = result.replace(/^-+/g, '');
 
