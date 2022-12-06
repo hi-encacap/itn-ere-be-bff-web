70d2444538f6e80aafa13064e7ee96538b7b60ab
Khanh Nguyen <encacap@gmail.com>
2022-12-06 20:19:11 +0700

    fix: get port from env instead of fixed value

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import { AppModule } from './app.module';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { ResponseInterceptor } from './common/interceptors/response.interceptor';
 import { LoggerService } from './common/modules/logger/logger.service';
+import AppConfigService from './configs/config.service';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const httpAdapter = app.get(HttpAdapterHost);
+  const configService = app.get(AppConfigService);
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   app.enableCors({
@@ -40,6 +42,6 @@ async function bootstrap() {
   );
   app.useGlobalInterceptors(new ResponseInterceptor());
 
-  await app.listen(3000);
+  await app.listen(configService.port);
 }
 bootstrap();
