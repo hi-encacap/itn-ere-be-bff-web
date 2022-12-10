aa8d9c3f78913e4137ba8f9fa85e57c50e2deb1f
Khanh Nguyen <encacap@gmail.com>
2022-12-10 14:45:42 +0700

    feat: search and sort feature

--- src/modules/contact/dto/query-contact-list.dto.ts
+++ src/modules/contact/dto/query-contact-list.dto.ts
@@ -1,5 +1,10 @@
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional } from 'class-validator';
 import { BaseQueryListParamsDto } from 'src/base/base.dto';
 
 export class QueryContactListDto extends BaseQueryListParamsDto {
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
   websiteId?: number;
 }
