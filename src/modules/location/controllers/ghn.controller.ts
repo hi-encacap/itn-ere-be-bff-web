76066a37fda04faf089019c611e91c0724f2afe9
Khanh Nguyen <encacap@gmail.com>
2022-12-21 21:36:29 +0700

    feat(ERE-10): exclude existed website provinces when get ghn provinces

--- src/modules/location/controllers/ghn.controller.ts
+++ src/modules/location/controllers/ghn.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { User } from 'src/common/decorators/user.decorator';
 import { JwtAuthGuard } from 'src/common/guards/jwt-auth.guard';
+import { IUser } from 'src/modules/user/interfaces/user.interface';
 import { GHNDistrictListQueryDto } from '../dtos/ghn-district-list-query.dto';
 import { GHNWardListQueryDto } from '../dtos/ghn-ward-list-query.dto';
 import { GHNService } from '../services/ghn.service';
@@ -10,8 +12,8 @@ export class GHNController {
   constructor(private readonly ghnService: GHNService) {}
 
   @Get('provinces')
-  getProvinces() {
-    return this.ghnService.getProvinces();
+  getProvinces(@User() user: IUser) {
+    return this.ghnService.getProvinces(user.websiteId);
   }
 
   @Get('districts')
