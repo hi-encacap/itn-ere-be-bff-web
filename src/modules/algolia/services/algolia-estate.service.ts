68b3c6b347d3b8f6e61a0a4a42d6604af3c86502
Khanh Nguyen <encacap@gmail.com>
2023-02-16 21:48:19 +0700

    fix: catch the error thrown by the algolia module

--- src/modules/algolia/services/algolia-estate.service.ts
+++ src/modules/algolia/services/algolia-estate.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import algoliasearch, { SearchClient, SearchIndex } from 'algoliasearch';
 import { isNil, omitBy } from 'lodash';
+import { LoggerService } from 'src/common/modules/logger/logger.service';
 import { AlgoliaConfigService } from 'src/configs/algolia/algolia-config.service';
 import AppConfigService from 'src/configs/config.service';
 import { IAlgoliaEstate } from '../interfaces/algolia.interface';
@@ -9,6 +10,7 @@ import { IAlgoliaEstate } from '../interfaces/algolia.interface';
 export class AlgoliaEstateService {
   private readonly client: SearchClient;
   private index: SearchIndex;
+  private logger = new LoggerService(AlgoliaEstateService.name);
 
   constructor(
     private readonly algoliaConfigService: AlgoliaConfigService,
@@ -16,17 +18,21 @@ export class AlgoliaEstateService {
   ) {
     this.client = algoliasearch(this.algoliaConfigService.appID, this.algoliaConfigService.apiKey);
     this.index = this.client.initIndex(`${this.appConfigService.envAlias}_estate`);
-    this.index.setSettings({
-      searchableAttributes: [
-        'title',
-        'description',
-        'address',
-        'wardName',
-        'districtName',
-        'provinceName',
-        'customId',
-      ],
-    });
+    this.index
+      .setSettings({
+        searchableAttributes: [
+          'title',
+          'description',
+          'address',
+          'wardName',
+          'districtName',
+          'provinceName',
+          'customId',
+        ],
+      })
+      .catch((error) => {
+        this.logger.error(error);
+      });
   }
 
   save(item: IAlgoliaEstate) {
