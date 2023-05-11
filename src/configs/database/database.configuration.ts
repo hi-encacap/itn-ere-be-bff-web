4fe67cafed13070e108ae47517c5846cf6dd31fb
Khanh Nguyen <encacap@gmail.com>
2023-05-11 15:08:43 +0700

    fix: update redis configs to be compatible with Railway

--- src/configs/database/database.configuration.ts
+++ src/configs/database/database.configuration.ts
@@ -10,10 +10,10 @@ export default registerAs('database', () => ({
     database: process.env.DB_POSTGRES_NAME,
   },
   redis: {
-    host: process.env.DB_REDIS_HOST,
-    port: process.env.DB_REDIS_PORT,
-    username: process.env.DB_REDIS_USERNAME,
-    password: process.env.DB_REDIS_PASSWORD,
+    host: process.env.REDISHOST || process.env.DB_REDIS_HOST,
+    port: process.env.REDISPORT || process.env.DB_REDIS_PORT,
+    username: process.env.REDISUSER || process.env.DB_REDIS_USERNAME,
+    password: process.env.REDISPASSWORD || process.env.DB_REDIS_PASSWORD,
     database: process.env.DB_REDIS_DATABASE,
   },
 }));
