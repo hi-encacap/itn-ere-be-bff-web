085525f04d1b8b095adc8b794b970072700d00d1
Khanh Nguyen <encacap@gmail.com>
2023-06-20 23:47:16 +0700

    ci: fix migration syntax issues

--- src/migrations/1687235667350-CategoryRefactoring.ts
+++ src/migrations/1687235667350-CategoryRefactoring.ts
@@ -3,7 +3,7 @@ import { MigrationInterface, QueryRunner } from 'typeorm';
 export class CategoryRefactoring1687235667350 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "categories" RENAME COLUMN "thumbnail_id" TO "avatar"');
-    await queryRunner.query('ALTER TABLE "categories" REMOVE COLUMN "parent_id"');
+    await queryRunner.query('ALTER TABLE "categories" DROP COLUMN "parent_id"');
     await queryRunner.query('ALTER TABLE "categories" ADD "left" integer NOT NULL');
     await queryRunner.query('ALTER TABLE "categories" ADD "right" integer NOT NULL');
     await queryRunner.query(
