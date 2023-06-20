45153c181ff0985cef6acccabe0e2257ce2de1d5
Khanh Nguyen <encacap@gmail.com>
2023-06-20 23:48:59 +0700

    ci: fix migration syntax issues

--- src/migrations/1687235667350-CategoryRefactoring.ts
+++ src/migrations/1687235667350-CategoryRefactoring.ts
@@ -4,11 +4,13 @@ export class CategoryRefactoring1687235667350 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query('ALTER TABLE "categories" RENAME COLUMN "thumbnail_id" TO "avatar"');
     await queryRunner.query('ALTER TABLE "categories" DROP COLUMN "parent_id"');
-    await queryRunner.query('ALTER TABLE "categories" ADD "left" integer NOT NULL');
-    await queryRunner.query('ALTER TABLE "categories" ADD "right" integer NOT NULL');
+    await queryRunner.query('ALTER TABLE "categories" ADD "left" integer');
+    await queryRunner.query('ALTER TABLE "categories" ADD "right" integer');
     await queryRunner.query(
       'UPDATE "categories" SET "left" = 1, "right" = 2 WHERE "left" IS NULL AND "right" IS NULL',
     );
+    await queryRunner.query('ALTER TABLE "categories" ALTER COLUMN "left" SET NOT NULL');
+    await queryRunner.query('ALTER TABLE "categories" ALTER COLUMN "right" SET NOT NULL');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
